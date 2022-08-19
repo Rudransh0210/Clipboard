51d75c8a57bb21ecbd7c1a39f5a35c78cfa9bdd4
Rudransh <jauharirudransh@gmail.com>
2022-08-19 16:57:42 +0530

    bfix: text transfer

--- Backend/back.py
+++ Backend/back.py
@@ -5,11 +5,19 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route('/', methods=[])
-def postcontent():
+@app.route('/post', methods=["POST"])
+def setcontent():
     content = request.form['clipboard_content']
-    return (content)
+    text_file = open('D:\Clipboard\Backend')
+    text_file.write(content)
+    text_file.close()
+
+@app.route('/get', methods= ["POST"])
+def postcontent():
+    text_file = open('D:\Clipboard\Backend')
+    content = text_file.read()
+    text_file.close()
+    return(content)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
-
